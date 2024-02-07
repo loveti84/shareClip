b7ac05f6f6e0e9fd861cfea4586e776b62fb1b2f
Lucas Daems <lucas.daems@student.kdg.be>
2024-02-07 21:24:31 +0100

    wtest

--- tst/tst.py
+++ tst/tst.py
@@ -1,10 +1,8 @@
 import socket
 
 # Bluetooth address of the receiving device
-def send():
-    target_address = "D0:39:57:F1:E7:92"
-    target_address = "04:7F:0E:7D:D0:D9"
-    port = 1
+def server(target_address,port):
+
 
     # Create a Bluetooth socket
     client_socket = socket.socket(socket.AF_BLUETOOTH, socket.SOCK_STREAM, socket.BTPROTO_RFCOMM)
@@ -19,33 +17,34 @@ def send():
         client_socket.sendall(message.encode())
         print("Sent:", message)
 
-    finally:
-        # Close the socket
-        client_socket.close()
-
-def rec():
-    port = 1
-
-    # Create a Bluetooth socket
-    server_socket = socket.socket(socket.AF_BLUETOOTH, socket.SOCK_STREAM, socket.BTPROTO_RFCOMM)
-
-    try:
-        # Bind the socket to the local Bluetooth adapter and specified port
-        server_socket.bind(("", port))
-        print("Listening for connections on RFCOMM channel", port)
-
-        # Listen for incoming connections
-        server_socket.listen(1)
-
-        # Accept the connection
-        client_socket, address = server_socket.accept()
-        print("Accepted connection from", address)
-
-        # Receive data
         data = client_socket.recv(1024)
         print("Received:", data.decode())
 
     finally:
-        # Close the sockets
+        # Close the socket
         client_socket.close()
-        server_socket.close()
+
+def client(target_address,port):
+
+    # Create a Bluetooth socket
+    server_socket = socket.socket(socket.AF_BLUETOOTH, socket.SOCK_STREAM, socket.BTPROTO_RFCOMM)
+
+
+    # Bind the socket to the local Bluetooth adapter and specified port
+    server_socket.bind((target_address, port))
+    print("Listening for connections on RFCOMM channel", port)
+
+    # Listen for incoming connections
+    server_socket.listen(1)
+
+    # Accept the connection
+    client_socket, address = server_socket.accept()
+    print("Accepted connection from", address)
+
+    # Receive data
+    data = client_socket.recv(1024)
+    print("Received:", data.decode())
+
+target_address = "D0:39:57:F1:E7:92"
+#target_address = "04:7F:0E:7D:D0:D9"
+server(target_address,2)
