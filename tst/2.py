cd77390d38d43d8d7556b1c9bf15a0c6a4c113d9
Lucas Daems <lucas.daems@student.kdg.be>
2024-02-07 23:09:55 +0100

    tt

--- tst/2.py
+++ tst/2.py
@@ -1,7 +1,6 @@
 import socket
 import threading
-import time
-import bluetooth
+
 
 # Bluetooth address of the receiving device
 def client(target_address,port):
@@ -9,13 +8,15 @@ def client(target_address,port):
 
     # Create a Bluetooth socket
     client_socket = socket.socket(socket.AF_BLUETOOTH, socket.SOCK_STREAM, socket.BTPROTO_RFCOMM)
-    client_socket = bluetooth.BluetoothSocket(bluetooth.RFCOMM)
-    client_socket.connect((target_address, port))
 
     # Connect to the target device
-
+    client_socket.connect((target_address, port))
     print("Connected to", target_address)
 
+    # Send data
+    message = "Hello, world!"
+    client_socket.send(message.encode())
+    print("Sent:", message)
 
     return  client_socket
 
@@ -24,11 +25,15 @@ def server(target_address,port):
     # Create a Bluetooth socket
     server_socket = socket.socket(socket.AF_BLUETOOTH, socket.SOCK_STREAM, socket.BTPROTO_RFCOMM)
 
-    bluetooth.BluetoothSocket(bluetooth.RFCOMM)
+
+    # Bind the socket to the local Bluetooth adapter and specified port
     server_socket.bind((target_address, port))
+    print("Listening for connections on RFCOMM channel", port)
+
+    # Listen for incoming connections
     server_socket.listen(1)
 
-
+    # Accept the connection
     client_socket, address = server_socket.accept()
     print("Accepted connection from", address)
 
@@ -42,16 +47,16 @@ def l(s):
         data = s.recv(1024)
         print("Received:", data.decode())
 
+
+
 target_address = "04:7F:0E:7D:D0:D9"
 target_address = "D0:39:57:F1:E7:92"
 
-
-s=client(target_address.lower(),3)
+s=server(target_address.lower(),3)
 t=threading.Thread(target=lambda :l(s))
 t.start()
 c=0
 while 1:
     i=f"test{c}"
-    time.sleep(1)
     c=+1
     s.send(i.encode())
